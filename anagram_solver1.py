9db5757057c9f07f5d551412cddc44daf834453b
Yuka <yuka.couweleers@gmail.com>
2021-05-16 12:50:18 -0400

    updating file

--- anagram_solver1.py
+++ anagram_solver1.py
@@ -10,22 +10,15 @@ def anagram_maker(str1, str2):
 	keys1 = dict1.keys()
 	keys2 = dict2.keys()
 
-	count1 = len(keys1)
-	count2 = len(keys2)
+	count1 = len(str1)
+	count2 = len(str2)
 
 	set1 = set(keys1)
 	common_keys = len(set1.intersection(keys2))
 
 	if common_keys == 0:
-		return count1 + count2
+		return (count1 + count2)
 	else:
-		return ((count1 + count2) - (common_keys * 2))
+		return (abs(count1 - count2))
 
 print(anagram_maker(str1, str2))
-
-
-
-# if __name__ == "__main__":
-#	str1 = 'yuka'
-#	str2 = 'chris'
-#	print (anagram_maker(str1, str2))
